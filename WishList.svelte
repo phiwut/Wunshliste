bd6e2fb97d9b6214553a88768ba6f4a8724538fb
Philipp Wüthrich <phiwut@users.noreply.github.com>
2020-11-28 13:22:58 +0100

    Initial commit

---
+++ WishList.svelte
@@ -0,0 +1,20 @@
+<script>
+                let wishList = [
+                  { text: "Write my first post", status: true },
+                  { text: "Upload the post to the blog", status: false },
+                  { text: "Publish the post at Facebook", status: false }
+                ];
+</script>
+
+{#each wishList as wish, index}
+	<span class:checked={wish.status}>{wish.text}</span>
+	<input bind:checked={wish.status} type="checkbox">
+	<br/>
+{/each} 
+
+
+<style>
+ .checked {
+   text-decoration: line-through;
+ }
+</style> 
